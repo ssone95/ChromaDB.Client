207b8c0af7f11a86cf36336f5560817ca02e26b7
Jiri Cincura <jiri@cincura.net>
2024-09-30 15:49:08 +0200

    Implement `Modify`.

---
+++ ChromaDB.Client/Models/Requests/CollectionModifyRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ChromaDB.Client.Models.Requests;
+
+public class CollectionModifyRequest
+{
+	[JsonPropertyName("name")]
+	public string? Name { get; init; }
+
+	[JsonPropertyName("metadata")]
+	public IDictionary<string, object>? Metadata { get; init; }
+}
