4e04468fb7cc9ac5868647d19a91d03736c481ae
Jiri Cincura <jiri@cincura.net>
2025-01-24 11:06:36 +0100

    Add support for X-Chroma-Token.

--- ChromaDB.Client/ChromaConfigurationOptions.cs
+++ ChromaDB.Client/ChromaConfigurationOptions.cs
@@ -7,16 +7,18 @@ public class ChromaConfigurationOptions
 	public Uri Uri { get; init; }
 	public string? Tenant { get; init; }
 	public string? Database { get; init; }
+	public string? ChromaToken { get; init; }
 
-	public ChromaConfigurationOptions(Uri uri, string? defaultTenant = null, string? defaultDatabase = null)
+	public ChromaConfigurationOptions(Uri uri, string? defaultTenant = null, string? defaultDatabase = null, string? chromaToken = null)
 	{
 		Uri = uri;
 		Tenant = defaultTenant;
 		Database = defaultDatabase;
+		ChromaToken = chromaToken;
 	}
 
-	public ChromaConfigurationOptions(string uri, string? defaultTenant = null, string? defaultDatabase = null)
-		: this(new Uri(uri), defaultTenant, defaultDatabase)
+	public ChromaConfigurationOptions(string uri, string? defaultTenant = null, string? defaultDatabase = null, string? chromaToken = null)
+		: this(new Uri(uri), defaultTenant, defaultDatabase, chromaToken)
 	{ }
 
 	public ChromaConfigurationOptions()
--- ChromaDB.Client/Common/ClientConstants.cs
+++ ChromaDB.Client/Common/ClientConstants.cs
@@ -7,6 +7,7 @@ internal static class ClientConstants
 	public const string DefaultTenantName = "default_tenant";
 	public const string DefaultDatabaseName = "default_database";
 	public const string DefaultUri = "http://localhost:8000/api/v1/";
+	public const string ChromaTokenHeader = "X-Chroma-Token";
 
 	public static ChromaTenant DefaultTenant { get; } = new(DefaultTenantName);
 	public static ChromaDatabase DefaultDatabase { get; } = new(DefaultDatabaseName);
